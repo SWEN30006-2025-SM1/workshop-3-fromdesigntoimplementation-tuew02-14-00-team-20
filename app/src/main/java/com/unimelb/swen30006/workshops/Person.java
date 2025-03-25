6f4296c8b9705e3d9d0191df5ee46a9ce090121a
carlvincet <carlvincet@student.unimelb.edu.au>
2025-03-25 15:10:49 +1100

    Completed Person

--- app/src/main/java/com/unimelb/swen30006/workshops/Person.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Person.java
@@ -1,6 +1,35 @@
 package com.unimelb.swen30006.workshops;
 
-public abstract class Person {
-    private static
+import java.util.Date;
 
+public abstract class Person {
+    private String address;
+    private final Date dob;
+    private String name;
+
+    public Person(String address, Date dob, String name) {
+        this.address = address;
+        this.dob = dob;
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
