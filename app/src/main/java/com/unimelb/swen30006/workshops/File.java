685cad4a6d85f2f94107e84c37abf3933d7e2383
carlvincet <carlvincet@student.unimelb.edu.au>
2025-03-25 15:37:20 +1100

    Fixed Submission, File

--- app/src/main/java/com/unimelb/swen30006/workshops/File.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/File.java
@@ -1,7 +1,29 @@
 package com.unimelb.swen30006.workshops;
 
+import java.util.Date;
+
 // Sample private file class to be replaced by your implementaiton
 class File {
+    private Date createdDate;
+    private String fileData;
+    private String fileName;
+
+    public File(Date createdDate, String fileData, String fileName) {
+        this.createdDate = createdDate;
+        this.fileData = fileData;
+        this.fileName = fileName;
+    }
+
+    public String content() {
+        /* Implementation */
+        return null;
+    }
+
+    public Integer fileSize() {
+        /* Implementation */
+        return null;
+    }
+
     public String fileType(){
         double num = Math.random();
         if(num<0.5){
@@ -9,5 +31,29 @@ class File {
         } else {
             return "docx";
         }
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getFileData() {
+        return fileData;
+    }
+
+    public void setFileData(String fileData) {
+        this.fileData = fileData;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
     }
 }
